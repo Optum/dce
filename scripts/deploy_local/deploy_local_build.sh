eb541b1e28f98fefe39cd8c4fbe0c41c0f079706
Kevin DeJong <kddejong@amazon.com>
2019-08-23 15:00:35 -0500

    Deploy local requires a lambda zip file when calling deploy
    Signed-off-by: Kevin DeJong <kddejong@amazon.com>

--- scripts/deploy_local/deploy_local_build.sh
+++ scripts/deploy_local/deploy_local_build.sh
@@ -11,6 +11,7 @@ KEY="local-tf-state"
 REGION="us-east-1"
 TABLE="local-tf-state"
 NAMESPACE=$(whoami)
+BUILD_ARTIFACT="bin/build_artifacts.zip"
 
 cd scripts/deploy_local && terraform init
 terraform apply -var="namespace=$NAMESPACE"
@@ -20,4 +21,4 @@ terraform init -backend-config="bucket=$BUCKET" -backend-config="key=$KEY"
 terraform apply -var="namespace=$NAMESPACE"
 ARTBUCKET=$(terraform output artifacts_bucket_name)
 cd ../
-scripts/deploy.sh $NAMESPACE $ARTBUCKET
+scripts/deploy.sh $BUILD_ARTIFACT $NAMESPACE $ARTBUCKET
