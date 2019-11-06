f340cffe047ede85a208331677941ed81f0abd79
AmudaPalani <amudhaselvapalani@gmail.com>
2019-11-06 15:22:42 -0600

    Validate logic moved to separate file

--- cmd/lambda/leases/create.go
+++ cmd/lambda/leases/create.go
@@ -4,17 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
-	"math"
-	"net/http"
-	"time"
-
 	"github.com/Optum/dce/pkg/api/response"
 	"github.com/Optum/dce/pkg/common"
 	"github.com/Optum/dce/pkg/db"
 	"github.com/Optum/dce/pkg/provision"
 	"github.com/Optum/dce/pkg/usage"
 	"github.com/aws/aws-lambda-go/events"
+	"log"
+	"net/http"
 )
 
 // CreateController is responsible for handling API events for creating leases.
@@ -48,44 +45,13 @@ type createLeaseRequest struct {
 func (c CreateController) Call(ctx context.Context, req *events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
 	// Extract the Body from the Request
-	requestBody := &createLeaseRequest{}
-	var err error
-	if req.HTTPMethod != "GET" {
-		err = json.Unmarshal([]byte(req.Body), requestBody)
-		if err != nil || requestBody.PrincipalID == "" {
-			log.Printf("Failed to Parse Request Body: %s", req.Body)
-			return response.ClientBadRequestError(fmt.Sprintf("Failed to Parse Request Body: %s", req.Body)), nil
-		}
+	requestBody, err := validateLeaseRequest(c, req)
+	if err != nil {
+		return response.BadRequestError(err.Error()), nil
 	}
 
 	principalID := requestBody.PrincipalID
 	log.Printf("Provisioning Account for Principal %s", principalID)
-
-	// Just do a quick sanity check on the request and make sure that the
-	// requested lease end date, if specified, is at least greater than
-	// today and if it isn't then return an error response
-	if requestBody.ExpiresOn != 0 && requestBody.ExpiresOn <= time.Now().Unix() {
-		errStr := fmt.Sprintf("Requested lease has a desired expiry date less than today: %d", requestBody.ExpiresOn)
-		log.Printf(errStr)
-		return response.BadRequestError(errStr), nil
-	}
-
-	// Check requested lease budget amount is greater than MAX_LEASE_BUDGET_AMOUNT. If it's, then throw an error
-	if requestBody.BudgetAmount > *c.MaxLeaseBudgetAmount {
-		errStr := fmt.Sprintf("Requested lease has a budget amount of %f, which is greater than max lease budget amount of %f", math.Round(requestBody.BudgetAmount), math.Round(*c.MaxLeaseBudgetAmount))
-		log.Printf(errStr)
-		return response.BadRequestError(errStr), nil
-	}
-
-	// Check requested lease budget period is greater than MAX_LEASE_BUDGET_PERIOD. If it's, then throw an error
-	currentTime := time.Now()
-	maxLeaseExpiresOn := currentTime.Add(time.Second * time.Duration(*c.MaxLeasePeriod))
-
-	if requestBody.ExpiresOn > maxLeaseExpiresOn.Unix() {
-		errStr := fmt.Sprintf("Requested lease has a budget expires on of %d, which is greater than max lease period of %d", requestBody.ExpiresOn, maxLeaseExpiresOn.Unix())
-		log.Printf(errStr)
-		return response.BadRequestError(errStr), nil
-	}
 
 	// Check if the principal has any existing Active/FinanceLock/ResetLock
 	// Leases
@@ -101,32 +67,6 @@ func (c CreateController) Call(ctx context.Context, req *events.APIGatewayProxyR
 		return response.ConflictError(errStr), nil
 	}
 	log.Printf("Principal %s has no Active Leases\n", principalID)
-
-	//Get sum of total spent by PrinicpalID for current billing period
-	usageStartTime := getBeginningOfCurrentBillingPeriod(*c.PrincipalBudgetPeriod)
-	usageEndTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 23, 59, 59, 0, time.UTC)
-
-	usageRecords, err := c.UsageSvc.GetUsageByDateRange(usageStartTime, usageEndTime)
-	if err != nil {
-		log.Printf("Failed to retrieve usage: %s", err)
-		return response.ServerErrorWithResponse(fmt.Sprintf("Failed to retrieve usage : %s",
-			err)), nil
-	}
-
-	// Group by PrincipalID to get sum of total spent for current billing period
-	spent := 0.0
-	for _, usage := range usageRecords {
-		log.Printf("usage records retrieved: %v", usage)
-		if usage.PrincipalID == principalID {
-			spent = spent + usage.CostAmount
-		}
-	}
-
-	if spent > *c.PrincipalBudgetAmount {
-		errStr := fmt.Sprintf("Unable to create lease: User principal %s has already spent %f of their weekly principal budget", principalID, math.Round(*c.PrincipalBudgetAmount))
-		log.Printf(errStr)
-		return response.BadRequestError(errStr), nil
-	}
 
 	// Get the First Ready Account
 	// Exit if there's an error or no ready accounts
@@ -247,19 +187,4 @@ func rollbackProvision(prov provision.Provisioner, err error,
 
 	// Return an error
 	return response.ServerErrorWithResponse(string(message))
-}
-
-// getBeginningOfCurrentBillingPeriod returns starts of the billing period based on budget period
-func getBeginningOfCurrentBillingPeriod(input string) time.Time {
-	currentTime := time.Now()
-	if input == "WEEKLY" {
-
-		for currentTime.Weekday() != time.Sunday { // iterate back to Sunday
-			currentTime = currentTime.AddDate(0, 0, -1)
-		}
-
-		return time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, time.UTC)
-	}
-
-	return time.Date(currentTime.Year(), currentTime.Month(), 1, 0, 0, 0, 0, time.UTC)
 }
---
+++ cmd/lambda/leases/validate.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"log"
+	"math"
+	"time"
+)
+
+// validateLeaseRequest validates lease budget amount and period
+func validateLeaseRequest(controller CreateController, req *events.APIGatewayProxyRequest) (*createLeaseRequest, error) {
+
+	// Validate body from the Request
+	requestBody := &createLeaseRequest{}
+	var err error
+	if req.HTTPMethod != "GET" {
+		err = json.Unmarshal([]byte(req.Body), requestBody)
+		if err != nil || requestBody.PrincipalID == "" {
+			errStr := fmt.Sprintf("Failed to Parse Request Body: %s", req.Body)
+			return requestBody, errors.New(errStr)
+		}
+	}
+
+	// Validate requested lease end date is greater than today
+	if requestBody.ExpiresOn != 0 && requestBody.ExpiresOn <= time.Now().Unix() {
+		errStr := fmt.Sprintf("Requested lease has a desired expiry date less than today: %d", requestBody.ExpiresOn)
+		return requestBody, errors.New(errStr)
+	}
+
+	// Validate requested lease budget amount is less than MAX_LEASE_BUDGET_AMOUNT
+	if requestBody.BudgetAmount > *controller.MaxLeaseBudgetAmount {
+		errStr := fmt.Sprintf("Requested lease has a budget amount of %f, which is greater than max lease budget amount of %f", math.Round(requestBody.BudgetAmount), math.Round(*controller.MaxLeaseBudgetAmount))
+		return requestBody, errors.New(errStr)
+	}
+
+	// Validate requested lease budget period is less than MAX_LEASE_BUDGET_PERIOD
+	currentTime := time.Now()
+	maxLeaseExpiresOn := currentTime.Add(time.Second * time.Duration(*controller.MaxLeasePeriod))
+	if requestBody.ExpiresOn > maxLeaseExpiresOn.Unix() {
+		errStr := fmt.Sprintf("Requested lease has a budget expires on of %d, which is greater than max lease period of %d", requestBody.ExpiresOn, maxLeaseExpiresOn.Unix())
+		return requestBody, errors.New(errStr)
+	}
+
+	// Validate requested lease budget amount is less than PRINCIPAL_BUDGET_AMOUNT for current principal billing period
+	usageStartTime := getBeginningOfCurrentBillingPeriod(*controller.PrincipalBudgetPeriod)
+	usageEndTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 23, 59, 59, 0, time.UTC)
+
+	usageRecords, err := controller.UsageSvc.GetUsageByDateRange(usageStartTime, usageEndTime)
+	if err != nil {
+		errStr := fmt.Sprintf("Failed to retrieve usage: %s", err)
+		return requestBody, errors.New(errStr)
+	}
+
+	// Group by PrincipalID to get sum of total spent for current billing period
+	spent := 0.0
+	for _, usage := range usageRecords {
+		log.Printf("usage records retrieved: %v", usage)
+		if usage.PrincipalID == requestBody.PrincipalID {
+			spent = spent + usage.CostAmount
+		}
+	}
+
+	if spent > *controller.PrincipalBudgetAmount {
+		errStr := fmt.Sprintf("Unable to create lease: User principal %s has already spent %f of their weekly principal budget", requestBody.PrincipalID, math.Round(*controller.PrincipalBudgetAmount))
+		return requestBody, errors.New(errStr)
+	}
+
+	return requestBody, nil
+}
+
+// getBeginningOfCurrentBillingPeriod returns starts of the billing period based on budget period
+func getBeginningOfCurrentBillingPeriod(input string) time.Time {
+	currentTime := time.Now()
+	if input == "WEEKLY" {
+
+		for currentTime.Weekday() != time.Sunday { // iterate back to Sunday
+			currentTime = currentTime.AddDate(0, 0, -1)
+		}
+
+		return time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, time.UTC)
+	}
+
+	return time.Date(currentTime.Year(), currentTime.Month(), 1, 0, 0, 0, 0, time.UTC)
+}
