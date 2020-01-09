46a80951316c0c4da8fc0878a4be6f862bffff98
Kevin DeJong <kddejong@amazon.com>
2020-01-09 14:29:30 -0600

    Convert accounts GET controller to new data model

--- cmd/lambda/accounts/list.go
+++ cmd/lambda/accounts/list.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/Optum/dce/pkg/api"
-	"github.com/Optum/dce/pkg/errors"
 	"log"
 	"net/http"
+
+	"github.com/Optum/dce/pkg/api"
+	"github.com/Optum/dce/pkg/db"
+	"github.com/Optum/dce/pkg/errors"
 
 	"github.com/Optum/dce/pkg/api/response"
 )
@@ -32,4 +34,40 @@ func GetAllAccounts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = json.NewEncoder(w).Encode(accountResponses)
+}
+
+// GetAccountByStatus - Returns the accounts by status
+func GetAccountByStatus(w http.ResponseWriter, r *http.Request) {
+	// Fetch the accounts.
+	accountStatus := r.FormValue("accountStatus")
+	status, err := db.ParseAccountStatus(accountStatus)
+	if err != nil {
+		log.Print(err)
+		api.WriteAPIErrorResponse(w, errors.NewValidation(err.Error(), nil))
+		return
+	}
+
+	accounts, err := Dao.FindAccountsByStatus(status)
+	if err != nil {
+		errorMessage := fmt.Sprintf("Failed to query database: %s", err)
+		log.Print(errorMessage)
+		api.WriteAPIErrorResponse(w, errors.NewInternalServer(errorMessage, nil))
+		return
+	}
+
+	if len(accounts) == 0 {
+		api.WriteAPIErrorResponse(w, errors.NewNotFound("account", accountStatus))
+		return
+	}
+
+	// Serialize them for the JSON response.
+	accountResponses := []*response.AccountResponse{}
+
+	for _, a := range accounts {
+		acctRes := response.AccountResponse(*a)
+		accountResponses = append(accountResponses, &acctRes)
+	}
+
+	_ = json.NewEncoder(w).Encode(accountResponses)
+
 }
