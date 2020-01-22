8dd4993def881dbea9002b8ee6ed831939bd380d
Kevin DeJong <kddejong@amazon.com>
2020-01-22 09:54:49 -0600

    Amuda palani feature/leasepkg2 (#235)
    * add lease package
    
    Co-authored-by: Amudha Palani <53053396+AmudaPalani@users.noreply.github.com>

--- pkg/lease/model.go
+++ pkg/lease/model.go
@@ -1,5 +1,13 @@
 package lease
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Optum/dce/pkg/errors"
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
 // Lease is a type corresponding to a Lease
 // table record
 type Lease struct {
@@ -18,5 +26,83 @@ type Lease struct {
 	Metadata                 map[string]interface{} `json:"metadata,omitempty" dynamodbav:"Metadata,omitempty"`                                 // Arbitrary key-value metadata to store with lease object
 }
 
+// Validate the account data
+func (l *Lease) Validate() error {
+	err := validation.ValidateStruct(l,
+		validation.Field(&l.ID, validateID...),
+		validation.Field(&l.AccountID, validateAccountID...),
+		validation.Field(&l.PrincipalID, validatePrincipalID...),
+		validation.Field(&l.LastModifiedOn, validateInt64...),
+		validation.Field(&l.Status, validateStatus...),
+		validation.Field(&l.CreatedOn, validateInt64...),
+	)
+	if err != nil {
+		return errors.NewValidation("lease", err)
+	}
+	return nil
+}
+
 // Leases is a list of type Account
 type Leases []Lease
+
+// Status is a lease status type
+type Status string
+
+const (
+	// StatusEmpty status
+	StatusEmpty Status = ""
+	// StatusActive status
+	StatusActive Status = "Active"
+	// StatusInactive status
+	StatusInactive Status = "Inactive"
+)
+
+// String returns the string value of Status
+func (c Status) String() string {
+	return string(c)
+}
+
+// StringPtr returns a pointer to the string value of Status
+func (c Status) StringPtr() *string {
+	v := string(c)
+	return &v
+}
+
+// StatusPtr returns a pointer to the string value of Status
+func (c Status) StatusPtr() *Status {
+	v := c
+	return &v
+}
+
+// ParseStatus - parses the string into an account status.
+func ParseStatus(status string) (Status, error) {
+	switch strings.ToLower(status) {
+	case "active":
+		return StatusActive, nil
+	case "inactive":
+		return StatusInactive, nil
+	}
+	return StatusEmpty, fmt.Errorf("Cannot parse value %s", status)
+}
+
+// StatusReason provides consistent verbiage for lease status change reasons.
+type StatusReason string
+
+const (
+	// StatusReasonExpired means the lease has past its expiresOn date and therefore expired.
+	StatusReasonExpired StatusReason = "Expired"
+	// StatusReasonOverBudget means the lease is over its budgeted amount and is therefore reset/reclaimed.
+	StatusReasonOverBudget StatusReason = "OverBudget"
+	// StatusReasonOverPrincipalBudget means the lease is over its principal budgeted amount and is therefore reset/reclaimed.
+	StatusReasonOverPrincipalBudget StatusReason = "OverPrincipalBudget"
+	// StatusReasonDestroyed means the lease has been deleted via an API call or other user action.
+	StatusReasonDestroyed StatusReason = "Destroyed"
+	// StatusReasonActive means the lease is still active.
+	StatusReasonActive StatusReason = "Active"
+	// StatusReasonRolledBack means something happened in the system that caused the lease to be inactive
+	// based on an error happening and rollback occuring
+	StatusReasonRolledBack StatusReason = "Rollback"
+	// StatusReasonAccountOrphaned means that the health of the account was compromised.  The account has been orphaned
+	// which means the leases are also made Inactive
+	StatusReasonAccountOrphaned StatusReason = "LeaseAccountOrphaned"
+)
--- pkg/lease/service.go
+++ pkg/lease/service.go
@@ -1,23 +1,30 @@
 package lease
 
+import (
+	"time"
+
+	"github.com/Optum/dce/pkg/errors"
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
 // Writer put an item into the data store
 type Writer interface {
-	WriteLease(input *Lease, lastModifiedOn *int64) error
+	Write(input *Lease, lastModifiedOn *int64) error
 }
 
 // Deleter Deletes an item from the data store
 type Deleter interface {
-	DeleteLease(input *Lease) error
+	Delete(input *Lease) error
 }
 
 // SingleReader Reads an item information from the data store
 type SingleReader interface {
-	GetLeaseByID(leaseID string) (*Lease, error)
+	Get(leaseID string) (*Lease, error)
 }
 
 // MultipleReader reads multiple items from the data store
 type MultipleReader interface {
-	GetLeases(*Lease) (*Leases, error)
+	List(*Lease) (*Leases, error)
 }
 
 // Reader data Layer
@@ -48,9 +55,73 @@ type Manager interface {
 	Setup(adminRole string) error
 }
 
-// Service is a type corresponding to a Account table record
+// Service is a type corresponding to a Lease table record
 type Service struct {
 	dataSvc ReaderWriterDeleter
+}
+
+// Get returns a lease from ID
+func (a *Service) Get(ID string) (*Lease, error) {
+
+	new, err := a.dataSvc.Get(ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return new, err
+}
+
+// Save writes the record to the dataSvc
+func (a *Service) Save(data *Lease) error {
+	var lastModifiedOn *int64
+	now := time.Now().Unix()
+	if data.LastModifiedOn == nil {
+		lastModifiedOn = nil
+		data.CreatedOn = &now
+		data.LastModifiedOn = &now
+	} else {
+		lastModifiedOn = data.LastModifiedOn
+		data.LastModifiedOn = &now
+	}
+
+	err := data.Validate()
+	if err != nil {
+		return err
+	}
+	err = a.dataSvc.Write(data, lastModifiedOn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete finds a given lease and deletes it if it is not of status `Active`. Returns the lease.
+func (a *Service) Delete(data *Lease) error {
+
+	err := a.dataSvc.Delete(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// List Get a list of leases based on Principal ID
+func (a *Service) List(query *Lease) (*Leases, error) {
+	err := validation.ValidateStruct(query,
+		// ID has to be empty
+		validation.Field(&query.ID, validation.NilOrNotEmpty, validation.By(isNil)),
+	)
+	if err != nil {
+		return nil, errors.NewValidation("lease", err)
+	}
+
+	leases, err := a.dataSvc.List(query)
+	if err != nil {
+		return nil, err
+	}
+
+	return leases, nil
 }
 
 // NewServiceInput Input for creating a new Service
