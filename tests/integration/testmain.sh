d9164be8f3d38879b24681b8d10c48b1848d7571
Nathan A. Good <nathan.a.good@gmail.com>
2020-02-06 14:29:23 -0600

    WIP: Shell script so far asserts a successful deployment using the DCE CLI.

--- tests/integration/testmain.sh
+++ tests/integration/testmain.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
+#
+# testmain.sh
+# 
+
+set -o pipefail
 
 workdir=$(dirname $0)
-# tmpdir="/tmp/dce-tests-$(date +"%y%m%d%H%M")"
-tmpdir="/tmp/dce-tests"
+# Do something like this if the test output dir should be unique
+# tmpdir=/tmp/dce-tests-$(date +"%y%m%d%H%M")
+tmpdir=/tmp/dce-tests
 mkdir -p ${tmpdir}
-zip_file="${tmpdir}/dce-cli-$(date +"%y%m%d%H%M").zip"
-dce_config_file="${tmpdir}/dce-cfg.yml"
+zip_file=${tmpdir}/dce-cli-$(date +"%y%m%d%H%M").zip
+dce_config_file=${tmpdir}/dce-cfg.yml
 dce_opts="--config ${dce_config_file}"
+log_file="${tmpdir}/test.log"
+
+# Zero out the log file before doing anything else...
+cat /dev/null > ${log_file}
 
 function writeConfig() {
-    echo <<EOF > $1
+    cat <<EOF > $1
 api:
   host:
   basepath:
@@ -36,30 +46,83 @@ EOF
     return 0
 }
 
+# assertExists makes sure the given command exists on the path and is executable.
+# Like all of the other assert* functions, this will print a "Failed. ..."
+# message and exit
+# args:
+#     1 - the command name (eg., "whoami", "unzip")
 function assertExists() {
     path=$(command -v $1)
     if [[ ! -x "${path}" ]]; then
-        echo "Cannot find command \"${1}\", which is required. Exiting." >&2
+        echo "Failed. Cannot find command \"${1}\", which is required. Exiting." >&2
         exit -1
     fi
     return 0
 }
 
+# assertSuccess makes sure the given command succeeds
+# Like all of the other assert* functions, this will print a "Failed. ..."
+# message and exit
+# args:
+#     1 - The tag in the output for the logs. This is used later to find 
+#         messages in the output with assertInLog or assertNotInLog
+#     2.. - The command and all of its arguments
 function assertSuccess() {
 cat <<EOF >> "${tmpdir}/test.log" 2>&1
-Running command "$@": 
+Running command "${@:2}": 
 EOF
-    $@ >> "${tmpdir}/test.log" 2>&1
+    ${@:2} 2>&1 | sed -l "s/^/$1: /" >> "${tmpdir}/test.log" 
     if [[ $? -ne 0 ]]; then
-        echo "Failed. Error while trying to run \"$@\". check logs." 1>&2
+        echo "Failed. Error while trying to run \"${@:2}\". check logs." 1>&2
         exit -1
     fi
-    printTestResult "Success"  
+    printTestResult "Success"
     return 0
 }
 
-# There are a couple things to make sure the user has installed. Like curl and jq
+# assertInLog makes sure the given message is found in the test logs.
+# Like all of the other assert* functions, this will print a "Failed. ..."
+# message and exit
+# args:
+#     1 - The tag for the test output to use to filter the results
+#     2 - The value to look for in the logs
+function assertInLog() {
+    echo -n "Asserting message \"$2\" found in logs..."
+    echo -n "Asserting message \"$2\" found in logs..." >> "${tmpdir}/test.log" 2>&1
+    count=$(grep -e "^$1:" ${log_file} | grep "$2" | wc -l)
+    if [[ ${count} -eq 0 ]]; then
+        printTestResult "Failed"
+        echo "Failed. Expected message \"$2\" for step \"$1\" not found in logs." 1>&2
+        exit -1
+    elif [[ ${count} -gt 1 ]]; then
+        printTestResult "Failed"
+        echo "Failed. Expected message \"$2\" for step \"$1\" found more than once." 1>&2
+        exit -1
+    fi
+    printTestResult "Success"
+    return 0
+}
 
+# assertNotInLog makes sure the given message is NOT found in the test logs.
+# Like all of the other assert* functions, this will print a "Failed. ..."
+# message and exit
+# args:
+#     1 - The tag for the test output to use to filter the results
+#     2 - The value to look for to be NOT in the logs
+function assertNotInLog() {
+    echo -n "Asserting message \"$2\" NOT found in logs..."
+    echo -n "Asserting message \"$2\" NOT found in logs..." >> "${tmpdir}/test.log" 2>&1
+    count=$(grep -e "^$1:" ${log_file} | grep -i "$2" | wc -l)
+    if [[ ${count} -ge 1 ]]; then
+        printTestResult "Failed"
+        echo "Failed. Message \"$2\" for step \"$1\" found but not expected." 1>&2
+        exit -1
+    fi
+    printTestResult "Success"
+    return 0
+}
+
+# There are a couple things to make sure the user has installed. Like curl and jq (maybe?)
 assertExists "curl"
 assertExists "unzip"
 # assertExists "jq"
@@ -89,18 +152,27 @@ if [[ ! -x "${dce_cmd}" ]]; then
 fi
 rm -f "${zip_file}"
 
-writeConfig "${dce_config_file}"
+writeConfig ${dce_config_file}
 
 #------------------------------------------------------------------------------
 # Test 1 - Make sure command works and usage works
 #------------------------------------------------------------------------------
 printTestBanner "Get help"
-assertSuccess "${dce_cmd}" --help
+assertSuccess "help" "${dce_cmd}" --help
+assertInLog "help" "Disposable Cloud Environment"
+assertInLog "help" "Usage:"
 
 #------------------------------------------------------------------------------
-# Test 1 - Test deployment
+# Test 2 - Test deployment
 #------------------------------------------------------------------------------
 printTestBanner "Deploy"
-assertSuccess "${dce_cmd}" system deploy -b ${dce_opts}
+assertSuccess "deploy" "${dce_cmd}" system deploy -b ${dce_opts}
+assertNotInLog "deploy" "fail"
+assertInLog "deploy" "Initializing"
+assertInLog "deploy" "Creating DCE infrastructure"
+
+
+# TODO: Currently, the test does not clean up after itself. Not certain if that
+# should be a part of this script or should be something different.
 
 exit 0
