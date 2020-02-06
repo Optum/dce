69d98b4c1b06130bbbf0291a5217d0d3b3028c08
Nathan A. Good <nathan.a.good@gmail.com>
2020-02-06 08:24:21 -0600

    WIP: initial version of test script through at least deploying DCE via CLI.

--- tests/integration/testmain.sh
+++ tests/integration/testmain.sh
@@ -1,22 +1,39 @@
 #!/usr/bin/env bash
 
 workdir=$(dirname $0)
-zipfile="/tmp/dce-cli-$(date +"%y%m%d%H%M").zip"
+# tmpdir="/tmp/dce-tests-$(date +"%y%m%d%H%M")"
+tmpdir="/tmp/dce-tests"
+mkdir -p ${tmpdir}
+zip_file="${tmpdir}/dce-cli-$(date +"%y%m%d%H%M").zip"
+dce_config_file="${tmpdir}/dce-cfg.yml"
+dce_opts="--config ${dce_config_file}"
+
+function writeConfig() {
+    echo <<EOF > $1
+api:
+  host:
+  basepath:
+region: us-east-1
+EOF
+
+}
 
 function printTestBanner() {
-cat >> "${workdir}/test.log" 2>&1 <<EOF
+cat <<EOF >> "${tmpdir}/test.log" 2>&1
 ###############################################################################
-Running test: "$1": 
-EOF    
+Running test: "$1" 
+EOF
     echo -n "Running test: \"${1}\"..."
+    return 0
 }
 
 function printTestResult() {
-cat >> "${workdir}/test.log" 2>&1 <<EOF
+cat <<EOF >> "${tmpdir}/test.log" 2>&1
 ...$1!
-###############################################################################
-EOF 
-    echo "...$1"
+-------------------------------------------------------------------------------
+EOF
+    echo "...$1!"
+    return 0
 }
 
 function assertExists() {
@@ -25,17 +42,19 @@ function assertExists() {
         echo "Cannot find command \"${1}\", which is required. Exiting." >&2
         exit -1
     fi
+    return 0
 }
 
-function assertExitZ() {
-cat >> "${workdir}/test.log" 2>&1 <<EOF
+function assertSuccess() {
+cat <<EOF >> "${tmpdir}/test.log" 2>&1
 Running command "$@": 
 EOF
-    "$@" >> "${workdir}/test.log" 2>&1
+    $@ >> "${tmpdir}/test.log" 2>&1
     if [[ $? -ne 0 ]]; then
-        echo "FAIL! Error while trying to run \"$@\". check logs." 1>&2
+        echo "Failed. Error while trying to run \"$@\". check logs." 1>&2
         exit -1
     fi
+    printTestResult "Success"  
     return 0
 }
 
@@ -57,23 +76,31 @@ else
     dce_bin_url="https://github.com/Optum/dce-cli/releases/download/${DCE_CLI_VERSION}/dce_darwin_amd64.zip"
 fi
 
-curl -p# -o ${zipfile} -L ${dce_bin_url}
+curl -p# -o ${zip_file} -L ${dce_bin_url}
 
 echo "Finished downloading; extracting file..."
 
-(cd $(dirname "${zipfile}") && unzip -q -o "${zipfile}")
+(cd $(dirname "${zip_file}") && unzip -q -o "${zip_file}")
 
-dce_cmd=/tmp/dce
+dce_cmd=$(dirname "${zip_file}")/dce
 
 if [[ ! -x "${dce_cmd}" ]]; then
-    chomd u+x "${dce_cmd}"
+    chmod u+x "${dce_cmd}"
 fi
+rm -f "${zip_file}"
+
+writeConfig "${dce_config_file}"
 
 #------------------------------------------------------------------------------
 # Test 1 - Make sure command works and usage works
 #------------------------------------------------------------------------------
 printTestBanner "Get help"
-assertExitZ "${dce_cmd}" --help
-printTestResult "Success"
+assertSuccess "${dce_cmd}" --help
+
+#------------------------------------------------------------------------------
+# Test 1 - Test deployment
+#------------------------------------------------------------------------------
+printTestBanner "Deploy"
+assertSuccess "${dce_cmd}" system deploy -b ${dce_opts}
 
 exit 0
