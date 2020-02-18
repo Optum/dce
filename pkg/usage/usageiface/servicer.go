c7f6f69d32c780395fe6c15d999df3cc2a65f7fe
AmudaPalani <amudhaselvapalani@gmail.com>
2020-02-18 14:07:30 -0600

    Added usage service build:

---
+++ pkg/usage/usageiface/servicer.go
@@ -0,0 +1,12 @@
+//
+
+package usageiface
+
+import (
+	"github.com/Optum/dce/pkg/usage"
+)
+// Servicer makes working with the Usage Service struct easier
+type Servicer interface {
+	// Get returns usage from startDate for input principalID
+	Get(startDate int64, principalID string) (*usage.Usage, error)
+}
