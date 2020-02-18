0618f9cd0f5644bf857353d26e9168453c5831e5
Joshua Marsh <joshmrsh@amazon.com>
2020-02-18 10:18:33 -0600

    linting

--- cmd/codebuild/reset/main.go
+++ cmd/codebuild/reset/main.go
@@ -142,8 +142,8 @@ func nukeAccount(svc *service, isDryRun bool) error {
 
 	// Print the contents of the config file, for logging/debugging
 	/*
-	#nosec: This value is derived from env vars. I.e. it is not populated with data from external users.
-	 */
+		#nosec: This value is derived from env vars. I.e. it is not populated with data from external users.
+	*/
 	conf, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		return err
