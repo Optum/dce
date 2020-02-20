c3ca5da3501f9e51966cf326b1ce8c04220e00b1
Joshua Marsh <joshmrsh@amazon.com>
2020-02-20 15:42:39 -0600

    changed logging text

--- pkg/api/user.go
+++ pkg/api/user.go
@@ -36,7 +36,7 @@ type User struct {
 func (u *User) Authorize(principalID string) error {
 	var err error
 	if u.Role != AdminGroupName && principalID != u.Username {
-		err = errors.NewUnathorizedError(fmt.Sprintf("User [%s] with role: [%s] attempted to act on a lease for: [%s], but was not authorized",
+		err = errors.NewUnathorizedError(fmt.Sprintf("User [%s] with role: [%s] attempted to act on a lease for [%s], but was not authorized",
 			u.Username, u.Role, principalID))
 	}
 	return err
