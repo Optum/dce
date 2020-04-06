5fdf529546cdf92b28da14e276751b257ffe1ce8
Edan Schwartz <edanschwartz@gmail.com>
2020-04-06 16:39:16 -0500

    Unit test coverage for usage service

--- pkg/usage/service_test.go
+++ pkg/usage/service_test.go
@@ -1,6 +1,7 @@
 package usage_test
 
 import (
+	errors2 "errors"
 	"fmt"
 	"testing"
 	"time"
@@ -81,5 +82,200 @@ func TestUpsertLeaseUsage(t *testing.T) {
 			err := usageSvc.UpsertLeaseUsage(&tt.req)
 			assert.True(t, errors.Is(err, tt.exp), "actual error %q doesn't match expected error %q", err, tt.exp)
 		})
+	}
+}
+
+func TestGetLease(t *testing.T) {
+
+	tests := []struct {
+		name             string
+		leaseID          string
+		mockLeaseUsage   *usage.Lease
+		dataServiceError error
+		expectedReturn   *usage.Lease
+		expectedError    error
+	}{
+		{
+			name:           "should return a lease from the data service",
+			leaseID:        "mock-lease-id",
+			mockLeaseUsage: &usage.Lease{LeaseID: ptrString("mock-lease-id")},
+			expectedReturn: &usage.Lease{LeaseID: ptrString("mock-lease-id")},
+		},
+		{
+			name:             "should return an error from the data service",
+			leaseID:          "mock-lease-id",
+			dataServiceError: errors2.New("data service error"),
+			expectedReturn:   nil,
+			expectedError:    errors2.New("data service error"),
+		},
+	}
+
+	for _, tt := range tests {
+		mockDataSvc := &mocks.LeaseReaderWriter{}
+
+		// Mock the lease record from the DB
+		mockDataSvc.
+			On("Get", tt.leaseID).
+			Return(tt.mockLeaseUsage, tt.dataServiceError)
+
+		usageSvc := usage.NewService(usage.NewServiceInput{
+			DataLeaseSvc: mockDataSvc,
+		})
+
+		res, err := usageSvc.GetLease(tt.leaseID)
+		assert.Equal(t, tt.expectedReturn, res)
+		assert.Equal(t, tt.expectedError, err)
+	}
+}
+
+func TestGetPrincipal(t *testing.T) {
+
+	tests := []struct {
+		name                     string
+		principalID              string
+		principalBudgetStartDate time.Time
+		mockPrincipalUsage       *usage.Principal
+		dataServiceError         error
+		expectedReturn           *usage.Principal
+		expectedError            error
+	}{
+		{
+			name:                     "should return a principal usage record from the data service",
+			principalID:              "mock-principal-id",
+			principalBudgetStartDate: time.Unix(100, 0),
+			mockPrincipalUsage:       &usage.Principal{PrincipalID: ptrString("mock-principal-id")},
+			expectedReturn:           &usage.Principal{PrincipalID: ptrString("mock-principal-id")},
+		},
+		{
+			name:                     "should return an error from the data service",
+			principalID:              "mock-principal-id",
+			principalBudgetStartDate: time.Unix(100, 0),
+			dataServiceError:         errors2.New("data service error"),
+			expectedReturn:           nil,
+			expectedError:            errors2.New("data service error"),
+		},
+	}
+
+	for _, tt := range tests {
+		mockDataSvc := &mocks.PrincipalReader{}
+
+		// Mock the lease record from the DB
+		mockDataSvc.
+			On("Get", tt.principalID, tt.principalBudgetStartDate).
+			Return(tt.mockPrincipalUsage, tt.dataServiceError)
+
+		usageSvc := usage.NewService(usage.NewServiceInput{
+			DataPrincipalSvc: mockDataSvc,
+		})
+
+		res, err := usageSvc.GetPrincipal(tt.principalID, tt.principalBudgetStartDate)
+		assert.Equal(t, tt.expectedReturn, res)
+		assert.Equal(t, tt.expectedError, err)
+	}
+}
+
+func TestListPrincipal(t *testing.T) {
+
+	tests := []struct {
+		name                string
+		mockPrincipalUsages usage.Principals
+		dataServiceError    error
+		expectedReturn      usage.Principals
+		expectedError       error
+	}{
+		{
+			name: "should return principal usage records from the data service",
+			mockPrincipalUsages: usage.Principals(
+				[]usage.Principal{
+					{PrincipalID: ptrString("mock-principal-id-1")},
+					{PrincipalID: ptrString("mock-principal-id-2")},
+				},
+			),
+			expectedReturn: usage.Principals(
+				[]usage.Principal{
+					{PrincipalID: ptrString("mock-principal-id-1")},
+					{PrincipalID: ptrString("mock-principal-id-2")},
+				},
+			),
+		},
+		{
+			name:             "should return an error from the data service",
+			dataServiceError: errors2.New("data service error"),
+			expectedReturn:   nil,
+			expectedError:    errors2.New("data service error"),
+		},
+	}
+
+	for _, tt := range tests {
+		mockDataSvc := &mocks.PrincipalReader{}
+
+		query := &usage.Principal{PrincipalID: ptrString("mock-id")}
+
+		// Mock the lease record from the DB
+		mockDataSvc.
+			On("List", query).
+			Return(&tt.mockPrincipalUsages, tt.dataServiceError)
+
+		usageSvc := usage.NewService(usage.NewServiceInput{
+			DataPrincipalSvc: mockDataSvc,
+		})
+
+		res, err := usageSvc.ListPrincipal(query)
+		if tt.expectedError != nil {
+			assert.Equal(t, tt.expectedError, err)
+		} else {
+			assert.Equal(t, &tt.expectedReturn, res)
+		}
+	}
+}
+
+func TestLeases(t *testing.T) {
+
+	tests := []struct {
+		name             string
+		mockLeaseUsages  usage.Leases
+		dataServiceError error
+		expectedReturn   usage.Leases
+		expectedError    error
+	}{
+		{
+			name: "should return lease usage records from the data service",
+			mockLeaseUsages: usage.Leases([]usage.Lease{
+				{LeaseID: ptrString("lease-1")},
+				{LeaseID: ptrString("lease-2")},
+			}),
+			expectedReturn: usage.Leases([]usage.Lease{
+				{LeaseID: ptrString("lease-1")},
+				{LeaseID: ptrString("lease-2")},
+			}),
+		},
+		{
+			name:             "should return an error from the data service",
+			dataServiceError: errors2.New("data service error"),
+			expectedReturn:   nil,
+			expectedError:    errors2.New("data service error"),
+		},
+	}
+
+	for _, tt := range tests {
+		mockDataSvc := &mocks.LeaseReaderWriter{}
+
+		query := &usage.Lease{LeaseID: ptrString("mock-id")}
+
+		// Mock the lease record from the DB
+		mockDataSvc.
+			On("List", query).
+			Return(&tt.mockLeaseUsages, tt.dataServiceError)
+
+		usageSvc := usage.NewService(usage.NewServiceInput{
+			DataLeaseSvc: mockDataSvc,
+		})
+
+		res, err := usageSvc.ListLease(query)
+		if tt.expectedError != nil {
+			assert.Equal(t, tt.expectedError, err)
+		} else {
+			assert.Equal(t, &tt.expectedReturn, res)
+		}
 	}
 }
