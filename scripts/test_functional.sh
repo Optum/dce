630fb327d93695b8ab057f5c7a038f62396cfe1c
Jaya Nanda <jaya.nanda@optum.com>
2020-10-09 13:57:26 -0400

    bump up functional tests timeout

--- scripts/test_functional.sh
+++ scripts/test_functional.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 mkdir -p junit-report
 
 # Run functional tests
-go test -v ./tests/... -test.timeout 30m 2>&1 | tee >(go-junit-report > junit-report/functional.xml)
+go test -v ./tests/... -test.timeout 40m 2>&1 | tee >(go-junit-report > junit-report/functional.xml)
 
