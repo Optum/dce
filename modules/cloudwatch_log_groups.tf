22e18f9bd265a24d7884ed60cd7ec34fe8ee599f
Jimmy Huang <jimmy.huang@observeinc.com>
2023-09-07 23:46:06 -0700

    turned cloud_watch_log_groups_list to cloud_watch_log_groups_set

--- modules/cloudwatch_log_groups.tf
+++ modules/cloudwatch_log_groups.tf
@@ -1,7 +1,7 @@
 module "cloudwatch_log-group" {
   source   = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
   version  = "=3.3.0"
-  for_each = local.cloudwatch_log_groups_list
+  for_each = local.cloudwatch_log_groups_set
 
   name              = each.value
   create            = false
