78820e78c55e253014578b2a78d69167ba2284cb
Jimmy Huang <jimmy.huang@observeinc.com>
2023-09-11 16:58:36 -0700

    NS => CNAME

--- modules/route53.tf
+++ modules/route53.tf
@@ -17,7 +17,7 @@ resource "aws_route53_record" "dce_ns" {
 resource "aws_route53_record" "dce" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "dce.observe-blunderdome.com"
-  type    = "NS"
+  type    = "CNAME"
   ttl     = "30"
   records = [
     aws_api_gateway_stage.api.invoke_url
