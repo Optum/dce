c9ac9122e1c98e93d30caa5e355c22a4b0b2f6e3
Jimmy Huang <jimmy.huang@observeinc.com>
2023-09-11 16:54:53 -0700

    start turning the api gateway url human-friendly

---
+++ modules/route53.tf
@@ -0,0 +1,25 @@
+resource "aws_route53_zone" "main" {
+  name = "observe-blunderdome.com"
+}
+
+resource "aws_route53_zone" "dce" {
+  name = "dce.observe-blunderdome.com"
+}
+
+resource "aws_route53_record" "dce_ns" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "dce.observe-blunderdome.com"
+  type    = "NS"
+  ttl     = "30"
+  records = aws_route53_zone.dce.name_servers
+}
+
+resource "aws_route53_record" "dce" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "dce.observe-blunderdome.com"
+  type    = "NS"
+  ttl     = "30"
+  records = [
+    aws_api_gateway_stage.api.invoke_url
+  ]
+}
