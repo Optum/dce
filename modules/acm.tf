21579731a59bdebe593d2fb5202c4364633b3390
Jimmy Huang <jimmy.huang@observeinc.com>
2023-09-16 22:24:01 -0700

    start to cover manually created resources with terraform

--- modules/acm.tf
+++ modules/acm.tf
@@ -3,14 +3,17 @@ data "aws_route53_zone" "observe_blunderdome" {
   private_zone = false
 }
 
+data "aws_api_gateway_domain_name" "custom" {
+  domain_name = aws_api_gateway_rest_api.gateway_api
+}
+
 resource "aws_route53_record" "custom" {
   zone_id = aws_route53_zone.observe_blunderdome.zone_id
   name    = "${var.custom_record_name}.${var.custom_zone_name}"
   type    = "A"
 
   alias {
-    name                   = 
-    zone_id                = aws_elb.main.zone_id
+    zone_id                = data.aws_api_gateway_domain_name.regional_zone_id
     evaluate_target_health = true
   }
 }
