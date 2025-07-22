39ac19a1d76f90aa7f0b624fb17d7f4bb55c3582
dilip0515 <dilipkumar.indias@gmail.com>
2025-07-22 10:13:35 -0500

    added tags

--- modules/list_dirty_accounts.tf
+++ modules/list_dirty_accounts.tf
@@ -1,20 +1,38 @@
-resource "aws_lambda_function" "list_dirty_accounts" {
-  function_name = "list-dirty-accounts-${var.namespace}"
-  handler       = "list_dirty_accounts"
-  runtime       = "provided.al2023"
-  role          = module.lambda.execution_role_arn
-  filename      = "${path.module}/lambda_stub.zip"  
+# resource "aws_lambda_function" "list_dirty_accounts" {
+#   function_name = "list-dirty-accounts-${var.namespace}"
+#   handler       = "list_dirty_accounts"
+#   runtime       = "provided.al2023"
+#   role          = module.lambda.execution_role_arn
+#   filename      = "${path.module}/lambda_stub.zip"  
 
-  environment {
-    variables = {
-      ACCOUNT_DB = aws_dynamodb_table.accounts.id
-      BUCKET     = aws_s3_bucket.artifacts.id
-      S3_KEY     = "dirty_accounts.csv"
-      NAMESPACE  = var.namespace
-    }
+#   environment {
+#     variables = {
+#       ACCOUNT_DB = aws_dynamodb_table.accounts.id
+#       BUCKET     = aws_s3_bucket.artifacts.id
+#       S3_KEY     = "dirty_accounts.csv"
+#       NAMESPACE  = var.namespace
+#     }
+#   }
+
+#   tags = var.global_tags
+# }
+module "list_dirty_accounts_lambda" {
+  source          = "./lambda"
+  name            = "list-dirty-accounts-${var.namespace}"
+  namespace       = var.namespace
+  description     = "Handles API requests to the /list dirty accounts endpoint"
+  global_tags     = var.global_tags
+  handler         = "list_dirty_accounts"
+  alarm_topic_arn = aws_sns_topic.alarms_topic.arn
+
+  environment = {
+    DEBUG              = "false"
+    BUCKET             = aws_s3_bucket.artifacts.id
+    S3_KEY             = "dirty_accounts.csv"
+    NAMESPACE          = var.namespace
+    AWS_CURRENT_REGION = var.aws_region
+    ACCOUNT_DB         = aws_dynamodb_table.accounts.id
   }
-
-  tags = var.global_tags
 }
 
 resource "aws_cloudwatch_event_rule" "list_dirty_accounts_schedule" {
@@ -26,24 +44,24 @@ resource "aws_cloudwatch_event_rule" "list_dirty_accounts_schedule" {
 resource "aws_cloudwatch_event_target" "list_dirty_accounts_lambda_target" {
   rule      = aws_cloudwatch_event_rule.list_dirty_accounts_schedule.name
   target_id = "list-dirty-accounts"
-  arn       = aws_lambda_function.list_dirty_accounts.arn
+  arn       = module.list_dirty_accounts_lambda.arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_invoke_list_dirty_accounts" {
 
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.list_dirty_accounts.function_name
+  function_name = module.list_dirty_accounts_lambda.name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.list_dirty_accounts_schedule.arn
 }
-# Lambda function to list dirty accounts and trigger necessary actions
-# This function will be scheduled to run weekly to check for dirty accounts
-module "lambda" {
-  source          = "./lambda"
-  namespace       = var.namespace
-  name            = "list-dirty-accounts"
-  description     = "Lambda function to list dirty accounts"
-  handler         = "list_dirty_accounts"
-   alarm_topic_arn = aws_sns_topic.alarms_topic.arn
-}
+# # Lambda function to list dirty accounts and trigger necessary actions
+# # This function will be scheduled to run weekly to check for dirty accounts
+# module "lambda" {
+#   source          = "./lambda"
+#   namespace       = var.namespace
+#   name            = "list-dirty-accounts"
+#   description     = "Lambda function to list dirty accounts"
+#   handler         = "list_dirty_accounts"
+#    alarm_topic_arn = aws_sns_topic.alarms_topic.arn
+# }
