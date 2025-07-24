5606dc850b5595989fecd89589c96143cd586934
dilip0515 <dilipkumar.indias@gmail.com>
2025-07-24 13:37:07 -0500

    address PR comments

--- modules/variables.tf
+++ modules/variables.tf
@@ -99,10 +99,7 @@ variable "budget_notification_from_email" {
   type    = string
   default = "notifications@example.com"
 }
-variable "aide_id" {
-  type        = string
-  description = "AIDE ID for global tags"
-}
+
 variable "budget_notification_bcc_emails" {
   type        = list(string)
   description = "Budget notifications emails will be bcc-d to these addresses"
